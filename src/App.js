675daccae37888a2bcb4b5d4720c09b4d0014cf8
paria340 <paria340@gmail.com>
2021-11-26 16:24:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+//importing libraries
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+
+//importing components
+import MainPage from './components/MainPage';
 
 function App() {
+
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<MainPage />}/>
+      </Routes>
+      
     </div>
+    </BrowserRouter>
   );
 }
 
