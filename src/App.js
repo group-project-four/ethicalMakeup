66fac6690c76879b0ac2eefaf38d357433697671
paria340 <paria340@gmail.com>
2021-11-27 14:19:14 -0800

    searchTab

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 //importing components
 import MainPage from './components/MainPage';
 import ProductPage from './components/ProductPage';
+import Catalogue from './components/Catalogue';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/*" element={<MainPage />} />
           <Route path="/:productID" element={<ProductPage />} />
+          <Route path="/catalogue" element={<Catalogue />} />
         </Routes>
       </div>
     </BrowserRouter>
