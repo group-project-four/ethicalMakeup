d3b7abdf5a0c273eb639123f6337b9e966339cd3
paria340 <paria340@gmail.com>
2021-11-27 22:46:38 -0800

    minor error handling

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -24,29 +24,50 @@ const MainPage = () => {
             }
         }).then((response) => {
             if(response.data.length !== 0){
-                // const firstTwenty = response.data.slice(0, 20)
-                // setProduct(firstTwenty)
-                setProduct(response.data)
+                console.log(response.data)
+                setProduct(response.data) 
                 setLoading(false)
+                setErrorMessage('')
             }else(
                 setErrorMessage('please enter a valid search term')
             )
         })
     }
 
+    const handleChangeOption = (event) => {
+        console.log(event.target.value)
+        if(event.target.value == 'alphabetical'){
+            const orderedProducts = currentPosts.sort((a, b) => {
+                return a.name > b.name
+            })
+        }
+    }
+
     const indexOfLastPost = currentPage * postsPerPage
     const indexOfFirstPost = indexOfLastPost - postsPerPage
     const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost)
-
     const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+    const filteredPrice = currentPosts.filter((productFiltered) => {
+        return productFiltered.price > 0.0
+    })
 
     return (
         <div>
             <ul>
             <NavbarTop handleQuery={handleQuery} />
+            {errorMessage}
+            <section>
+                <label htmlFor="sorting"></label>
+                <select name="sorting" id="sorting" onChange={handleChangeOption}>
+                    <option value="price">price</option>
+                    <option value="alphabetical">alphabetical</option>
+                    <option value="rating">rating</option>
+                </select>
+            </section>
             <ul>
                 {
-                    currentPosts.map(product => {
+                    filteredPrice.map(product => {
                         return (
                             <div key={product.id}>
                                 {product.name}
@@ -56,6 +77,7 @@ const MainPage = () => {
                                         alt={`product of ${product.brand} company`}
                                     />
                                 </Link>
+                                {product.price}{product.price_sign} 
                             </div>
                         )
                     })
@@ -66,8 +88,6 @@ const MainPage = () => {
             <Pagination postsPerPage={postsPerPage} totalPosts = {products.length} paginate={paginate}/>
 
             </ul>
-
-            {errorMessage}
         </div>
     )
 }
