d7fcffbe9234e76e2c3c0a9b941499239033c001
Dani <daniella.m.gagliardi@gmail.com>
2021-11-28 13:07:36 -0500

    added links to each product to route to the product-page

--- src/components/EyeProducts.js
+++ src/components/EyeProducts.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
 
 const EyeProducts = () => {
-     // Set all products from API to state
-     const [products, setProduct] = useState([]);
+    // Set all products from API to state
+    const [products, setProduct] = useState([]);
 
-     // Call API
-     useEffect(() => {
-         axios({
-             url: "http://makeup-api.herokuapp.com/api/v1/products.json",
-         }).then((response) => {
-             setProduct(response.data);
-             console.log(response.data);
-         });
-     }, []);
+    // Call API
+    useEffect(() => {
+        axios({
+            url: "http://makeup-api.herokuapp.com/api/v1/products.json",
+        }).then((response) => {
+            setProduct(response.data);
+            console.log(response.data);
+        });
+    }, []);
 
-     // Filter Products array for Mascara, EyeLiner & Eyeshadow:
+    // Filter Products array for Mascara, EyeLiner & Eyeshadow:
     const eyeProducts = products.filter(
         (products) =>
             products.product_type === "mascara" ||
@@ -37,15 +38,16 @@ const EyeProducts = () => {
             "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg";
     }
 
-    return(
+    return (
         <div>
             <h2>Eye</h2>
-                <div className="sectionCarousel">
-                    <ul>
-                        {eyeProductAdj.map((product) => {
-                            // let price = product.price
-                            // let roundedPrice = price.toFixed(2)
-                            return (
+            <div className="sectionCarousel">
+                <ul>
+                    {eyeProductAdj.map((product) => {
+                        // let price = product.price
+                        // let roundedPrice = price.toFixed(2)
+                        return (
+                            <Link to={`/${product.id}`}>
                                 <li key={product.id}>
                                     <img
                                         src={product.image_link}
@@ -55,10 +57,11 @@ const EyeProducts = () => {
                                     <h3>{product.name}</h3>
                                     <p>$ {product.price}</p>
                                 </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+                            </Link>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
