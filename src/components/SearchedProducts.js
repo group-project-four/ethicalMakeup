26a3c85cf30bcf7c40d4be03cbcf18b21c505432
Dani <daniella.m.gagliardi@gmail.com>
2021-11-29 20:34:26 -0500

    added $ sign infront of dollar amount instead of variable

--- src/components/SearchedProducts.js
+++ src/components/SearchedProducts.js
@@ -20,7 +20,7 @@ const SearchedProducts = (props) => {
                                         />
                                 </Link>
                                 <h3>{product.name}</h3>
-                                <p>{product.price_sign}{product.price}</p>
+                                <p>$ {product.price}</p>
                                 
                             </div>
                         )
