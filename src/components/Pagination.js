e88c8bbe0ce60b0232888709fdcf5a735aa008be
paria340 <paria340@gmail.com>
2021-12-01 10:23:19 -0800

    edit

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,10 +2,12 @@
 function Pagination({postsPerPage, totalPosts, paginate}) {
     const pageNumbers =[]
 
+    //a loop to number of pages and push i to the empty array 
     for(let i =1; i <= Math.ceil(totalPosts / postsPerPage); i++){
         pageNumbers.push(i)
     }
 
+    //mapping through page numbers and use the function paginate to set the current page number
     return(
         <nav className="pageNumebrs">
             {pageNumbers.map(number => 
